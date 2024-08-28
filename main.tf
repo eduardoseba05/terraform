7a10acaa31a6e53fa18d7ba92058f2cf789ea5f9
educamargo05 <eduardo.seba@sptech.school>
2024-08-28 18:31:15 -0300

    commit final da aula passada

--- main.tf
+++ main.tf
@@ -13,3 +13,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "ec2-terraform2-2" {
+  ami = "ami-066784287e358dad1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ec2-terraform2-separado-2"
+  }
+  root_block_device{
+    volume_size = 40
+}
+}
